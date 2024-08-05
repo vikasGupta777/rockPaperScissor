e5909e2eebec1e14221fefe9c6f8e52381136eb2
Vikas Gupta <vikasgupta16181320@gmail.com>
2024-08-05 21:17:14 +0530

    Made a plyaRound function to play game game once.
    Wrote a logic in the playRound function to play a game for one round.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
 
 function getComputerChoice(){
     let random =  Math.floor(Math.random() * 3);
-    alert(random);
     if( random == 0 ){
         return "Rock";
     }else if(random == 1){
@@ -13,9 +13,54 @@ function getComputerChoice(){
     }
 }
 
-function gethumanChoice(){
+function getHumanChoice(){
     let input = prompt(" Enter your choice from 'Rock' 'Paper' and 'Scissor' ");
-    input = input[0].toUpperCase() + input.slice(1);
     return input;
 }
+
+function playRound(humanChoice, computerChoice){
+
+    if  ((humanChoice == "Scissor" || humanChoice == "SCISSOR" || humanChoice == "scissor") && (computerChoice == "Rock")){
+        computerScore = computerScore+1;
+        return alert ("Computer win! Rock beats Scissor");
+
+    }else if((humanChoice == "Scissor" || humanChoice == "SCISSOR"|| humanChoice == "scissor") && (computerChoice == "Paper")){
+        humanScore = humanScore+1;
+        return alert("You win! Scissor beats Paper");
+
+    }else if((humanChoice == "Paper" || humanChoice == "paper" || humanChoice == "PAPER") && (computerChoice == "Rock")){
+        humanScore = humanScore+1;
+        return  alert(" You win! Paper beats Rock");
+
+    }else if((humanChoice == "Paper" || humanChoice == "paper" || humanChoice == "PAPER") && (computerChoice == "Scissor")){
+        computerScore++;
+        return alert(" Computer win! Scissor beats Paper");  
+
+    }else if ((humanChoice == "Rock " || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Paper")){
+        computerScore++;
+        return alert("Computer win! Paper beats Rock");
+
+    }else if ( (humanChoice == "Rock " || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Scissor")){
+        humanScore++;
+        return alert("You win! Rock beats Scissor");
+    }else {
+        tieScore++;
+        return alert("Match Tie");
+    }           
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
+
+
+
+
+
+
+
+
+
+
 
