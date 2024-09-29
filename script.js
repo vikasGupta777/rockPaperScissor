be8a670218a40f1b439b6e1ae6be53270c3a4c15
Rajan <131269059+rajan-357@users.noreply.github.com>
2024-09-29 13:08:12 +0530

    Some changes to javascript files Removed playgame function and added a button for player selection

--- script.js
+++ script.js
@@ -14,69 +14,124 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let input = prompt(" Enter your choice from 'Rock' 'Paper' and 'Scissor' ");
-    return input;
-}
-
 function playRound(humanChoice, computerChoice){
 
-    if  ((humanChoice == "Scissor" || humanChoice == "SCISSOR" || humanChoice == "scissor") && (computerChoice == "Rock")){
+    if (humanScore == 5  || computerScore == 5){
+        if(humanScore ==5){
+            result.textContent = " PLAYER WIN!!!!";
+            playerWins.textContent = "";
+            computerWins.textContent = "";
+        }else {
+            result.textContent = " COMPUTER WIN!!!";
+            playerWins.textContent = "";
+            computerWins.textContent = "";
+        }
+        
+    }
+    else if  ((humanChoice == "Scissor" || humanChoice == "SCISSOR" || humanChoice == "scissor") && (computerChoice == "Rock")){
         computerScore = computerScore+1;
-        return alert ("Computer win! Rock beats Scissor");
+        result.textContent = "Computer win! Rock beats Scissor";
+        computerWins.textContent = computerScore;
 
     }else if((humanChoice == "Scissor" || humanChoice == "SCISSOR"|| humanChoice == "scissor") && (computerChoice == "Paper")){
         humanScore = humanScore+1;
-        return alert("You win! Scissor beats Paper");
+        result.textContent =("You win! Scissor beats Paper");
+        playerWins.textContent = humanScore;
 
     }else if((humanChoice == "Paper" || humanChoice == "paper" || humanChoice == "PAPER") && (computerChoice == "Rock")){
         humanScore = humanScore+1;
-        return  alert(" You win! Paper beats Rock");
+        result.textContent =(" You win! Paper beats Rock");
+        playerWins.textContent = humanScore;
 
     }else if((humanChoice == "Paper" || humanChoice == "paper" || humanChoice == "PAPER") && (computerChoice == "Scissor")){
         computerScore++;
-        return alert(" Computer win! Scissor beats Paper");  
+        result.textContent =(" Computer win! Scissor beats Paper"); 
+        computerWins.textContent = computerScore; 
 
-    }else if ((humanChoice == "Rock " || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Paper")){
+    }else if ((humanChoice == "Rock" || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Paper")){
         computerScore++;
-        return alert("Computer win! Paper beats Rock");
+        result.textContent =("Computer win! Paper beats Rock");
+        computerWins.textContent = computerScore;
 
-    }else if ( (humanChoice == "Rock " || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Scissor")){
+    }else if ( (humanChoice == "Rock" || humanChoice == "rock" || humanChoice == "ROCK") && (computerChoice == "Scissor")){
         humanScore++;
-        return alert("You win! Rock beats Scissor");
+        result.textContent =("You win! Rock beats Scissor");
+        playerWins.textContent = humanScore;
     }else {
         tieScore++;
-        return alert("Match Tie");
+        result.textContent =("Match Tie");
     }           
 }
 
-function playGame(){
 
-    
-    let humanSelection = getHumanChoice();
+const main = document.createElement("div");
+document.body.appendChild(main);
+
+const mainHeader = document.createElement("div");
+main.appendChild(mainHeader);
+
+
+const rockBtn = document.createElement("button");
+rockBtn.innerText = " Rock"; 
+mainHeader.appendChild(rockBtn);
+
+const paperBtn = document.createElement("button");
+paperBtn.textContent = " Paper"; 
+mainHeader.appendChild(paperBtn);
+
+const scissorBtn = document.createElement ("button");
+scissorBtn.textContent = " Scissor"
+mainHeader.appendChild(scissorBtn);
+
+mainHeader.style.border = "2px solid red";
+mainHeader.style.alignItems = "center";
+mainHeader.style.justifyContent = "spaceAround";
+
+const result = document.createElement("div");
+result.style.border = "2px ";
+result.style.boxSizing = "border-box"
+result.style.height = "50px";
+result.style.width = "250px";
+main.appendChild(result);
+
+const playerWins = document.createElement("div");
+playerWins.style.border = "2px solid black";
+playerWins.style.boxSizing = "border-box"
+playerWins.style.height = "50px";
+playerWins.style.width = "125px";
+main.appendChild(playerWins);
+
+const computerWins = document.createElement("div");
+computerWins.style.border = "2px solid black"; 
+computerWins.style.boxSizing = "border-box"
+computerWins.style.height = "50px";
+computerWins.style.width = "125px";
+main.appendChild(computerWins);
+
+rockBtn.addEventListener("click",()=> {
+    let humanSelection = rockBtn.innerText;
     let computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+    console.log(humanSelection);
+    console.log(computerSelection);
 
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
+})
+
+paperBtn.addEventListener("click",()=> {
+    let humanSelection = paperBtn.innerText;
+    let computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
+    console.log(humanSelection);
+    console.log(computerSelection);
 
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
+})
+
+scissorBtn.addEventListener("click",()=>{
+    let humanSelection = scissorBtn.innerText;
+    let computerSelection = getComputerChoice();    
     playRound(humanSelection,computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-
-    alert(` Your Score ${ humanScore } Computer Score ${ computerScore} and Match tied ${tieScore} `);
-}
-
-playGame();
+   
+})
 
 
 
@@ -89,3 +144,103 @@ playGame();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getHumanChoice(){
+//     let input = prompt(" Enter your choice from 'Rock' 'Paper' and 'Scissor' ");
+//     return input;
+// }
+
+
+// function playGame(){
+
+    
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+
+//     humanSelection = getHumanChoice();
+//     computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+
+//     humanSelection = getHumanChoice();
+//     computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+
+//     humanSelection = getHumanChoice();
+//     computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+
+//     humanSelection = getHumanChoice();
+//     computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+
+//     alert(` Your Score ${ humanScore } Computer Score ${ computerScore} and Match tied ${tieScore} `);
+// }
+
+// playGame();
