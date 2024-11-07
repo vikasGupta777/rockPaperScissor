9155e9c29a08ca87c7a7697aa3454eb46f1b074e
Rajan <131269059+rajan-357@users.noreply.github.com>
2024-11-08 01:13:24 +0530

    add frontend to the game add frontend of the game and reacd little closer to final of a project.

--- script.js
+++ script.js
@@ -6,10 +6,34 @@ function getComputerChoice(){
     let random =  Math.floor(Math.random() * 3);
 
     if( random == 0 ){
+        const computerIcon = document.querySelector(".computer-icon");
+        let img = document.createElement("img");
+        img.src = "/images/rock.jpeg";
+        img.id="img";
+        img.style.height = '100%';
+        img.style.width = '100%';
+        computerIcon.appendChild(img); 
+
         return "Rock";
     }else if(random == 1){
+        const computerIcon = document.querySelector(".computer-icon");
+        let img = document.createElement("img");
+        img.src = "/images/paper.jpeg"
+        img.id="img";
+        img.style.height = '100%';
+        img.style.width = '100%';
+        computerIcon.appendChild(img); 
+
         return "Paper";
     }else{
+        const computerIcon = document.querySelector(".computer-icon");
+        let img = document.createElement("img");
+        img.src = "/images/scissor.jpeg";
+        img.id="img";
+        img.style.height = '100%';
+        img.style.width = '100%';
+        computerIcon.appendChild(img); 
+
         return "Scissor";  
     }
 }
@@ -64,73 +88,62 @@ function playRound(humanChoice, computerChoice){
 }
 
 
-const main = document.createElement("div");
-document.body.appendChild(main);
-
-const mainHeader = document.createElement("div");
-main.appendChild(mainHeader);
 
 
-const rockBtn = document.createElement("button");
-rockBtn.innerText = " Rock"; 
-mainHeader.appendChild(rockBtn);
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorBtn = document.querySelector("#scissorBtn")
+const result = document.querySelector(".result");
+const playerWins = document.querySelector(".player-score");
+const computerWins = document.querySelector(".computer-score");
 
-const paperBtn = document.createElement("button");
-paperBtn.textContent = " Paper"; 
-mainHeader.appendChild(paperBtn);
 
-const scissorBtn = document.createElement ("button");
-scissorBtn.textContent = " Scissor"
-mainHeader.appendChild(scissorBtn);
 
-mainHeader.style.border = "2px solid red";
-mainHeader.style.alignItems = "center";
-mainHeader.style.justifyContent = "spaceAround";
 
-const result = document.createElement("div");
-result.style.border = "2px ";
-result.style.boxSizing = "border-box"
-result.style.height = "50px";
-result.style.width = "250px";
-main.appendChild(result);
+rockBtn.addEventListener("click",()=>{
+    
 
-const playerWins = document.createElement("div");
-playerWins.style.border = "2px solid black";
-playerWins.style.boxSizing = "border-box"
-playerWins.style.height = "50px";
-playerWins.style.width = "125px";
-main.appendChild(playerWins);
+    const playerIcon = document.querySelector(".playerChoice-container");
+    let img = document.createElement("img");
+    img.src = "/images/rock.jpeg";
+    img.style.height = '100%';
+    img.style.width = '100%';
+    playerIcon.appendChild(img);  
 
-const computerWins = document.createElement("div");
-computerWins.style.border = "2px solid black"; 
-computerWins.style.boxSizing = "border-box"
-computerWins.style.height = "50px";
-computerWins.style.width = "125px";
-main.appendChild(computerWins);
-
-rockBtn.addEventListener("click",()=> {
-    let humanSelection = rockBtn.innerText;
+    let humanSelection = "rock";
     let computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
-    console.log(humanSelection);
-    console.log(computerSelection);
+
 
 })
 
-paperBtn.addEventListener("click",()=> {
-    let humanSelection = paperBtn.innerText;
+paperBtn.addEventListener("click",()=>{
+
+    const playerIcon = document.querySelector(".playerChoice-container");
+    let img = document.createElement("img");
+    img.src = "/images/paper.jpeg";
+    img.style.height = '100%';
+    img.style.width = '100%';
+    playerIcon.appendChild(img);  
+
+    let humanSelection = "paper";
     let computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
-    console.log(humanSelection);
-    console.log(computerSelection);
-
 })
 
 scissorBtn.addEventListener("click",()=>{
-    let humanSelection = scissorBtn.innerText;
-    let computerSelection = getComputerChoice();    
+
+    const playerIcon = document.querySelector(".playerChoice-container");
+    let img = document.createElement("img");
+    img.src = "/images/scissor.jpeg" ;
+    img.style.height = '100%';
+    img.style.width = '100%';
+    playerIcon.appendChild(img);  
+
+    let humanSelection = "scissor";
+    let computerSelection = getComputerChoice();
     playRound(humanSelection,computerSelection);
-   
+
 })
 
 
@@ -139,6 +152,128 @@ scissorBtn.addEventListener("click",()=>{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const main = document.createElement("div");
+// document.body.appendChild(main);
+
+// const mainHeader = document.createElement("div");
+// main.appendChild(mainHeader);
+
+
+// const rockBtn = document.createElement("button");
+// rockBtn.innerText = " Rock"; 
+// mainHeader.appendChild(rockBtn);
+
+// const paperBtn = document.createElement("button");
+// paperBtn.textContent = " Paper"; 
+// mainHeader.appendChild(paperBtn);
+
+// const scissorBtn = document.createElement ("button");
+// scissorBtn.textContent = " Scissor"
+// mainHeader.appendChild(scissorBtn);
+
+// mainHeader.style.border = "2px solid red";
+// mainHeader.style.alignItems = "center";
+// mainHeader.style.justifyContent = "spaceAround";
+
+// result.style.border = "2px ";
+// result.style.boxSizing = "border-box"
+// result.style.height = "50px";
+// result.style.width = "250px";
+// main.appendChild(result);
+
+// playerWins.style.border = "2px solid black";
+// playerWins.style.boxSizing = "border-box"
+// playerWins.style.height = "50px";
+// playerWins.style.width = "125px";
+// main.appendChild(playerWins);
+
+// computerWins.style.border = "2px solid black"; 
+// computerWins.style.boxSizing = "border-box"
+// computerWins.style.height = "50px";
+// computerWins.style.width = "125px";
+// main.appendChild(computerWins);
+
+// rockBtn.addEventListener("click",()=> {
+//     let humanSelection = rockBtn.innerText;
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+//     console.log(humanSelection);
+//     console.log(computerSelection);
+
+// })
+
+// paperBtn.addEventListener("click",()=> {
+//     let humanSelection = paperBtn.innerText;
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+//     console.log(humanSelection);
+//     console.log(computerSelection);
+
+// })
+
+// scissorBtn.addEventListener("click",()=>{
+//     let humanSelection = scissorBtn.innerText;
+//     let computerSelection = getComputerChoice();    
+//     playRound(humanSelection,computerSelection);
+   
+// })
 
 
 
